db6a698c864bc1a79db61a4426d4c9ac4d587f35
Willian Lima <willian.slima@mercadolivre.com>
2024-07-25 14:42:06 -0300

    wip

--- src/main/java/me/dio/williandsl/Santander/Application.java
+++ src/main/java/me/dio/williandsl/Santander/Application.java
@@ -1,4 +1,4 @@
-package me.dio.williandsl.Santander.dev_2024;
+package me.dio.williandsl.Santander;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
