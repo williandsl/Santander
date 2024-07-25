bddcd6fd0a4076472d2ddb4776d00bd1ae2bc4ab
Willian Lima <willian.slima@mercadolivre.com>
2024-07-25 12:34:34 -0300

    projectInicial

---
+++ src/main/java/me/dio/williandsl/Santander/dev_2024/SantanderDev2024Application.java
@@ -0,0 +1,13 @@
+package me.dio.williandsl.Santander.dev_2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderDev2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderDev2024Application.class, args);
+	}
+
+}
---
+++ src/test/java/me/dio/williandsl/Santander/dev_2024/SantanderDev2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.williandsl.Santander.dev_2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderDev2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
