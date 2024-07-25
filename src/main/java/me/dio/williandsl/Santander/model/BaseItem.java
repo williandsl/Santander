8294cfad9e6318a8e466275da8452e32b179589c
Willian Lima <willian.slima@mercadolivre.com>
2024-07-25 17:12:20 -0300

    wip

---
+++ src/main/java/me/dio/williandsl/Santander/model/BaseItem.java
@@ -0,0 +1,18 @@
+package me.dio.williandsl.Santander.model;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@MappedSuperclass
+public abstract class BaseItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String icon;
+    private String description;
+}
---
+++ src/main/java/me/dio/williandsl/Santander/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package me.dio.williandsl.Santander.repository;
+
+import me.dio.williandsl.Santander.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository <User, Long> {
+}
